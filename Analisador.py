a3a94c816fefe76f454be2084379fdd66ee05561
luan048 <luanmonteiropedrosa@gmail.com>
2024-11-26 21:55:09 -0300

    Add Resume Function with new Window

--- Analisador.py
+++ Analisador.py
@@ -4,30 +4,48 @@ from pytube import extract
 import pandas as pd
 
 import tkinter as tk
-from tkinter import simpledialog
+from tkinter import messagebox
+
+def functionResume_video():
+    try:
+        # Link do video pela entrada
+        link_videoYT = entrada.get()
+
+        # Valida link
+        if not link_videoYT:
+            raise ValueError("Nenhum link foi inserido...")
+
+        # Extra ID
+        video_Id = extract.video_id(link_videoYT)
+
+        srt=YouTubeTranscriptApi.get_transcript(video_Id, languages=['pt']) # Obtem legenda
+
+        lista = [linha for linha in srt]
+        df = pd.DataFrame(lista)
+
+        # Salva resumo em CSV
+        df.to_csv("dados.csv", encoding='utf-8', index=False)
+
+        saida.config(text="Resumo feito com sucesso!") #TEMPORARIO
+        print(df) # Temporario
+    
+    except Exception as e:
+        messagebox.showerror("Erro", f"Erro no processamento do vídeo: {e}")
 
 # Cria uma janela
-root = tk.Tk()
-root.withdraw()
+janela = tk.Tk()
+janela.title("Analisador de Videos")
 
-# Lista para leganda do video
-lista = []
+# Permite entrada do link
+tk.Label(janela, text="Digite o link do video:").pack()
+entrada = tk.Entry(janela, width=40)
+entrada.pack()
 
-link_videoYT = simpledialog.askstring("Analisador video", "Insira o link do video")
-video_Id = extract.video_id(link_videoYT) # Extrai legenda
+# Botão que inicia resumo
+botao = tk.Button(janela, text="Resumir", command=functionResume_video) #Colocar a função que faz o resumo do video
+botao.pack()
 
-srt = YouTubeTranscriptApi.get_transcript(video_Id, languages=['pt'])
+saida = tk.Label(janela, text="", fg="blue")
+saida.pack()
 
-# Repetição para pegar linha por linha
-for i in srt:
-    lista.append(i)
-
-# Cria o dataframe
-df = pd.DataFrame(lista)
-df
-
-# Baixa as transcrições
-df_download = df.to_csv("dados.csv", encoding= 'utf-8')
-df_download
-
-print(df) #Temporário
+janela.mainloop()
